65f421295f6e05dd3608df24af3537fa395d7b35
Carolyn <carolynlschroeder@hotmail.com>
2017-02-13 11:30:39 -0600

    First Commit

--- AsyncAwaitExample/Program.cs
+++ AsyncAwaitExample/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace AsyncAwaitExample
@@ -10,6 +11,34 @@ namespace AsyncAwaitExample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine($"Application thread ID: {Thread.CurrentThread.ManagedThreadId}");
+            var stuff = new AsyncAwaitDemo();
+            var t = stuff.DoStuff();
+            t.Wait();
+            Console.WriteLine($"Finished on thread ID: {Thread.CurrentThread.ManagedThreadId}");
+            Console.ReadLine();
+        }
+    }
+
+    public class AsyncAwaitDemo
+    {
+        public Task DoStuff()
+        {
+            return Task.Run(() =>
+            {
+                LongRunningOperation();
+            });
+        }
+
+        private void LongRunningOperation()
+        {
+            int counter;
+
+            for (counter = 0; counter < 50; counter++)
+            {
+                Console.WriteLine($"{counter} from Task thread ID: {Thread.CurrentThread.ManagedThreadId}");
+
+            }
         }
     }
 }
